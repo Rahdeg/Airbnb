93d9fc5b0ad43e7d3a78b101cadfc75e4be585f2
Rahdeg <walett95@gmail.com>
2022-11-16 13:30:05 +0100

    fix

--- src/Pages/Airbnb.jsx
+++ src/Pages/Airbnb.jsx
@@ -5,6 +5,7 @@ import IMG2 from '../assets/Group 4038.png';
 import IMG3 from '../assets/logo (1).png'
 import {Menu, About} from '../Components'
 
+
 const Airbnb = () => {
   return (
     <div className='flex flex-col  '>
@@ -15,7 +16,7 @@ const Airbnb = () => {
     <h2 className='font-bold text-4xl md:text-5xl text-cartBg   mt-4'><span className=' text-cardOverlay'>Home</span> in the <span className=' text-cardOverlay'>Metaverse</span></h2>
     </div>
     <p className='text-left text-lg font-medium mt-8 text-cartBg item-center '> we provide you access to luxury and affordable houses <br/>in the metaverse, get a chance to turn your<br/> imagination to reality at your comfort zone</p>
-    <div className='flex md:w-full w-full mt-20 outline outline-houtlineColor rounded-md items-center justify-between'>
+    <div className='flex mr-10 md:w-full w-full mt-20 outline outline-houtlineColor rounded-md items-center justify-between'>
     <input type='text' placeholder='search for location' className='h-80  p-2 outline-none '/>
     <button className=' bg-cardOverlay rounded-r-md   p-2 text-white text-sm md:w-150 w-[350px] h-80  md:h-full'>
     Search
